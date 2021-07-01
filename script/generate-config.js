d202a02567c9ab7eeaad7ece3c645351febe6b6f
manoj <manoj@no-reply-git.com>
2021-07-01 20:04:51 +0530

    commit changes

--- script/generate-config.js
+++ script/generate-config.js
@@ -6,20 +6,25 @@ const config = fs.readFileSync(path.join(__dirname, '../cypress.json'), { encodi
 
 // Adding the username and password from the variable group
 const configJson = JSON.parse(config);
+console.log(`Config: ${configJson}`)
 configJson.env = {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    clientid: process.env.CLIENTID,
-    clientsecret: process.env.CLIENTSECRET
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  clientid: process.env.CLIENTID,
+  clientsecret: process.env.CLIENTSECRET
 };
+
+// Rename the junit file based on the browser used
+configJson.reporterOptions.mochaFile = 'cypress/reports/junit.${process.env.BROWSER || "unknown"}-[hash].xml';
+
 // Check if there is a project ID specified
 if (process.env.CYPRESS_PROJECT_ID) {
-    configJson.projectId = process.env.CYPRESS_PROJECT_ID;
+  configJson.projectId = process.env.CYPRESS_PROJECT_ID;
 }
 
 // Logging the config
 if (process.env.VERBOSE) {
-    console.log(configJson);
+  console.log(configJson);
 }
 
 fs.writeFileSync(path.join(__dirname, '../cypress${process.env.CI ? "" : ".test"}.json'), JSON.stringify(configJson, null, 2));
